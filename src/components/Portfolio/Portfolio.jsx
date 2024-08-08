f153eed030015fe86ba69bdb7ea08b515cb86060
IamPP <159672683+PPJelly@users.noreply.github.com>
2024-08-08 22:25:50 +0700

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,17 +7,17 @@ export default function Portfolio() {
     <div className={styles.port_con}>
       <h3 className={styles.port_title}></h3>
       <div className={styles.port_list}>
-        <div className={styles.port_items}>
+        {/* <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://images.unsplash.com/photo-1561736778-92e52a7769ef?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Project Title</p></Tilt>
-        </div>
-        <div className={styles.port_items}>
+        </div> */}
+        {/* <div className={styles.port_items}>
             <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://images.unsplash.com/photo-1561736778-92e52a7769ef?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Project Title</p></Tilt>
-        </div>
-        <div className={styles.port_items}>
+        </div> */}
+        {/* <div className={styles.port_items}>
         <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://images.unsplash.com/photo-1561736778-92e52a7769ef?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Project Title</p></Tilt>
@@ -26,7 +26,7 @@ export default function Portfolio() {
         <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://images.unsplash.com/photo-1561736778-92e52a7769ef?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
             <p>Project Title</p></Tilt>
-        </div>
+        </div> */}
         <div className={styles.port_items}>
         <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://images.unsplash.com/photo-1561736778-92e52a7769ef?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
